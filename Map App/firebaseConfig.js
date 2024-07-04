ecefd7d5c5dc20bf7bf4822a9d3f2c4641fc3aad
Davidak2 <60138656+Davidak2@users.noreply.github.com>
2024-07-04 13:27:40 +0300

    Update firebaseConfig.js

--- Map App/firebaseConfig.js
+++ Map App/firebaseConfig.js
@@ -4,12 +4,7 @@ import "firebase/compat/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTthhhRg61y1jkMYdmfTN430vXPE3cVAA",
-  authDomain: "mapapp-6a22c.firebaseapp.com",
-  projectId: "mapapp-6a22c",
-  storageBucket: "mapapp-6a22c.appspot.com",
-  messagingSenderId: "909157957350",
-  appId: "1:909157957350:web:80017386d327e61f13c527"
+  /*Your API info Here*/
 };
 
 if(!firebase.apps.length)
